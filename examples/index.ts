28b19e3d1e6c8a699fc4b238731e8dbf1788d253
James Conkling <james.lane.conkling@gmail.com>
2019-10-13 18:05:58 -0400

    add router for multiple examples

--- examples/index.ts
+++ examples/index.ts
@@ -1,9 +1,15 @@
-import { createElement } from 'react'
+import { createElement as el } from 'react'
 import { render } from 'react-dom'
+import { Route, BrowserRouter, Link } from 'react-router-dom'
 import { TodoList } from './todoList'
 
 render((
-  createElement('div', null,
-    createElement('h1', null, 'Falcor Connect'),
-    createElement(TodoList, null))
+  el(BrowserRouter, {},
+    el('h1', null, el(Link, { to: '/' }, 'Falcor Connect')),
+    el(Route, { exact: true, path: '/' },
+      el('nav', null,
+        el('ul', null,
+          el('li', null, el(Link, { to: '/hoc' }, 'withFalcor HOC')),
+          el('li', null, el(Link, { to: '/abc' }, 'useFalcor Hook'))))),
+    el(Route, { path: '/hoc', component: TodoList }))
 ), document.getElementById('app'))
