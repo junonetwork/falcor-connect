eb77e1e5457432b0323e3e877ccb142890551c0f
James Conkling <james.lane.conkling@gmail.com>
2019-11-24 19:04:15 -0500

    allow useStream to be called with the same args

--- src/useStream.ts
+++ src/useStream.ts
@@ -2,50 +2,49 @@ import { useRef, useState, useLayoutEffect } from 'react'
 import { Observable, Subject } from 'rxjs'
 
 
-export const useStreamNaive = <T, R>(
-  project: (stream$: Observable<T>) => Observable<R>,
-  data: T,
-): R | undefined => {
-  let prev = useRef<T>()
-  const stream$ = useRef(new Subject<T>())
-  let [emit, setEmit] = useState<R>()
-
-  useLayoutEffect(() => {
-    const subscription = stream$.current.pipe(project).subscribe({
-      next: (next) => setEmit(next)
-    })
-
-    stream$.current.next(data)
-    return () => subscription.unsubscribe()
-  }, [])
-
-  if (prev.current !== data) {
-    emit = undefined
-    stream$.current.next(data)
-  }
-
-  prev.current = data
-
-  return emit
-}
-
-
+/**
+ * tests:
+ * - calling useStream on synchronous stream should return: data
+ *   - it should not return: undefined|data
+ *   - tests `synchronous.current = true; ... ; synchronous.current = false`
+ * - calling useStream twice on a delayed stream should return: undefined...data1...undefined...data2
+ *   - it should not return: undefined...data1...data1...data2
+ *   - tests `result.current = undefined`
+ * - calling useStream on a delayed stream should return: undefined...data1
+ *   - it should not return: undefined...data1...data1...data1...data1...
+ *   - tests infinite loop prevention where stream next emit triggers another event to be dispatched
+ *   - not sure if this is fixed.  see below implementation
+ * - calling useStream on a synchronous stream should return: data1...data2...data3
+ * 
+ * maybe test as a matrix against
+ * - stream types
+ *   - stream that emits once synchronous  :data
+ *   - stream that emits multiple times synchronously :data|data2|data3
+ *   - stream that emits synchronously and continues asynchronously :data...data2...data3
+ *   - stream that emits once asynchronously :...data1
+ *   - stream that emits asyncronously multiple times :...data1...data2...data3
+ * - call count/signature
+ *   - call once
+ *   - call multiple times w/ different arg
+ *   - call multiple times w/ the same arg
+ */
 export const useStream = <T, R>(
   project: (stream$: Observable<T>) => Observable<R>,
   data: T,
 ): R | undefined => {
-  const prev = useRef<T>()
-  const stream$ = useRef(new Subject<T>())
-  const emit = useRef<R>()
+  const result = useRef<R>()
   const synchronous = useRef(true)
+  const rerendering = useRef(false)
+  const stream$ = useRef(new Subject<T>())
   const [_, rerender] = useState(false)
 
   useLayoutEffect(() => {
     const subscription = stream$.current.pipe(project).subscribe({
       next: (next) => {
-        emit.current = next
+        result.current = next
         if (!synchronous.current) {
-          rerender((prev) => !prev)
+          rerendering.current = true
+          rerender((x) => !x)
         }
       }
     })
@@ -54,13 +53,13 @@ export const useStream = <T, R>(
     return () => subscription.unsubscribe()
   }, [])
 
-  synchronous.current = true
-  if (prev.current !== data) {
-    emit.current = undefined
+  if (!rerendering.current) {
+    synchronous.current = true
+    result.current = undefined
     stream$.current.next(data)
+    synchronous.current = false
   }
-  prev.current = data
-  synchronous.current = false
+  rerendering.current = false
 
-  return emit.current
+  return result.current
 }
